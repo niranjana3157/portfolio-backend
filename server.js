cbd1c78c0f36b270f62fddecd2045488c6ace817
Niranjana <niranjana31507@gmail.com>
2026-06-08 22:43:52 +0530

    Fix Render port

--- server.js
+++ server.js
@@ -37,6 +37,8 @@ app.post("/contact", async (req, res) => {
 });
 
 // 6. Server
-app.listen(3000, () => {
-    console.log("Server running on port 3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
